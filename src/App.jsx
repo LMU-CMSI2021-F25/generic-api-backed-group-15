1345e40f5d9a85f1697348f0be0dc1de2c94cc34
esiao125 <esiao020@gmail.com>
2025-10-08 12:14:59 -0700

    Simple web app

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,22 @@ import { useState, useEffect } from "react";
 export default function App() {
 const [info, setInfo] = useState("");
 
-useEffect(() => {
+const fetchDog = () => {
 fetch("https://dog.ceo/api/breeds/image/random")
     .then((r) => r.json())
     .then((response) => {
     setInfo(response.message);
     });
+};
+
+useEffect(() => {
+    fetchDog();
 }, []);
 
 return (
     <div>
         <img src={info} alt="Random dog" />
+        <button onClick={fetchDog}>Get Dog</button>
     </div>
     )
 }
